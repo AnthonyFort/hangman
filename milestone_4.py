3f0caf678aef283722c3ee540f95e7906031a914
Anthony Fort <ajf2195@columbia.edu>
2023-11-04 14:57:11 +0000

    Adds docstrings to Hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -2,7 +2,21 @@ import random
 
 class Hangman():
 
+  '''
+  This class records and updates the player's progress.
+
+  The attributes track how the player is doing while the methods update the state.
+  '''
+
   def __init__(self, word_list, num_lives=5):
+    '''
+    'word' is the word from word_list which has been randomly selected by Python.
+    'word_guessed' tracks the player's progress.
+    'num_letters' tracks the number of unique, undiscovered letters still remaining in 'word'.
+    'num_of_lives' tracks the number of lives the player has remaining (starting with 5).
+    'word_list' is the list of words from which Python selected a random word.
+    'list_of_guesses' records the guesses that the player has already made.
+    '''
     self.word = random.choice(word_list)
     self.word_guessed = list(map(lambda x: '_', list(self.word)))
     self.num_letters = len(set(self.word))
@@ -10,6 +24,13 @@ class Hangman():
     self.word_list = word_list
     self.list_of_guesses = []
 
+  '''
+  This method checks to see if a given guess matches with any letters in the chosen word.
+  If it does, blank spaces in the word_guessed list whose index matches instances of that letter in the word 
+  are replaced with that letter.
+  If there is a match num_letters is updated.
+  If there is not a match, the num_of_lives attribute is updated.
+  '''
   def check_guess(self, guess):
       guess = guess.lower()
       if guess in self.word:
@@ -25,6 +46,10 @@ class Hangman():
         print(f'You have {self.num_of_lives} lives left.')  
 
   def ask_for_input(self):
+    '''
+    This method asks the player to make a guess
+    and checks whether or not the guess is valid.
+    '''
     while True:
       guess = input('Please guess a letter: ')
       if len(guess) != 1 or not guess.isalpha():  
@@ -50,5 +75,5 @@ hang = Hangman(word_list)
 # print(hang.word_guessed)
 # print(hang.num_letters)
 
-hang.ask_for_input()
+help(Hangman)
 
