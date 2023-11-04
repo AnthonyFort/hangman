6ccd21c22fe1d64328c76b08f106232395b7990e
Anthony Fort <ajf2195@columbia.edu>
2023-11-04 14:32:00 +0000

    Adds ask_for_input method

--- milestone_4.py
+++ milestone_4.py
@@ -12,10 +12,19 @@ class Hangman():
 
   def check_guess(self, guess):
       guess = guess.lower()
-      print(guess)
       if guess in self.word:
         print(f'Good guess! {guess} is in the word')
 
+  def ask_for_input(self):
+    while True:
+      guess = input('Please guess a letter: ')
+      if len(guess) != 1 or not guess.isalpha():  
+        print('Invalid letter. Please, enter a single alphabetical character.')  
+      elif guess in self.list_of_guesses:
+        print('You already tried that letter!')
+      else:
+        self.check_guess(guess)  
+      self.list_of_guesses.append(guess)  
 
   def update_progress(self, guess):
     for letter in self.word:
@@ -38,4 +47,4 @@ hang = Hangman(word_list)
 # print(hang.word_guessed)
 # print(hang.num_letters)
 
-hang.check_guess('e')
+hang.ask_for_input()
