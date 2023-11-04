0c3555bbd59ce5b286759c588fabf18b875aaf4f
Anthony Fort <ajf2195@columbia.edu>
2023-11-04 12:29:54 +0000

    Adds condition to check if guess is in the chosen word.

--- milestone_2.py
+++ milestone_2.py
@@ -6,10 +6,13 @@ word = random.choice(word_list)
 while True:
   guess = input('Please provide a single letter: ')
   if len(guess) == 1 and guess.isalpha():
-    print('Good guess!')
-    break
+    if guess in word:
+      print(f'Good guess, {guess} is in the word.')
+      break
+    else:
+      print(f'Sorry, {guess} is not in the word. Try again')
   else:
     print('Invalid letter. Please, enter a single alphabetical character.')  
 
-
+print(word)
 
