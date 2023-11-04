ce874cabbac20d166bc1b8df4249778f0b89878e
Anthony Fort <ajf2195@columbia.edu>
2023-11-04 12:26:26 +0000

    Places input inside a while loop

--- milestone_2.py
+++ milestone_2.py
@@ -2,12 +2,14 @@ import random
 
 word_list = ['apple', 'orange', 'pear', 'cherry', 'strawberry']
 word = random.choice(word_list)
-guess = input('Please provide a single letter: ')
 
-if len(guess) == 1 and guess.isalpha():
-  print('Good guess!')
-else:
-  print('Oops! That is not a valid input.')  
+while True:
+  guess = input('Please provide a single letter: ')
+  if len(guess) == 1 and guess.isalpha():
+    print('Good guess!')
+    break
+  else:
+    print('Invalid letter. Please, enter a single alphabetical character.')  
 
 
 
